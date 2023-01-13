5ffce8699b6be1289f12b5ba32415ec72a207cb9
Alan-LR <alanlopes.r@gmail.com>
2023-01-13 11:39:13 -0300

    Aula 29 Inserindo dados no sistema - post/formulario

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Moment } from './../../interfaces/moment';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,5 +9,42 @@ import { Component, Input } from '@angular/core';
 })
 export class MomentFormComponent {
 
+  @Output() onSubmit = new EventEmitter<Moment>
   @Input() btnText!: string
+
+  momentForm!: FormGroup
+
+  constructor(private fb: FormBuilder){
+    
+  }
+  
+  ngOnInit(): void{
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      image: new FormControl(''),
+    });
+  }
+
+  getTitle(){
+    return this.momentForm.get('title')
+  }
+
+  getDescription(){
+    return this.momentForm.get('description')
+  }
+
+  onFileSelected(event: any){
+    const file: File = event.target.files[0]
+
+    this.momentForm.patchValue({image: file})
+  }
+
+
+  submit(){
+    console.log(this.momentForm.value)
+    this.onSubmit.emit(this.momentForm.value) // passando os dados do formulário filho para o elemento pai
+  }  
+
 }
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,3 +1,5 @@
+import { MomentService } from './../../../services/moment.service';
+import { Moment } from './../../../interfaces/moment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +10,27 @@ import { Component } from '@angular/core';
 export class NewMomentComponent {
 
   btnText = 'Compartilhar!'
+
+  constructor(private momentService: MomentService){}
+
+  async elementoPaiRecebeEventoFilho(moment: Moment){ //convertendo as informações que chegaram do elemento filho
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if(moment.image){
+      formData.append("image", moment.image)
+    }
+
+    //todo
+    await this.momentService.createMoment(formData).subscribe();
+    //enviar para o service
+
+    //exibir msg de sucesso
+
+    //redirecionar o usuário após concluir o formulário para outra página
+
+  }
 
 }
