d0e43e165f0a798d7f3150594c72a173589f7684
Alan-LR <alanlopes.r@gmail.com>
2023-01-16 15:12:55 -0300

    Aula 34 Busca de dados no Angular - pesquisa somente com front-end

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -11,19 +11,29 @@ export class HomeComponent {
   imagePath: string = "http://localhost:3333/";  //para carregarmos a imagem devemos passar uma url para o src, no caso o endereço do banco de dados e o nome da imagem(que foi passado no html)
   allMoments: Moment[] = []
   moments: Moment[] = []
+  searchTerm: string = '';
 
-  constructor(private momentService: MomentService){
-    
+  constructor(private momentService: MomentService) {
+
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.momentService.getMoments().subscribe((items) => {
       const data = items.data
-      data.map((item)=>{
+      data.map((item) => {
         item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR')
       })
       this.allMoments = data
       this.moments = data
+    });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement
+    const value = target.value.toLowerCase()
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value)
     });
   }
 
