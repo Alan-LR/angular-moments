86d907cdd789606d9e07800af510ddfcbc73d728
Alan-LR <alanlopes.r@gmail.com>
2023-01-17 10:14:33 -0300

    Aula 36  CSS página de Momentos

--- src/app/components/moment/moment.component.ts
+++ src/app/components/moment/moment.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./moment.component.scss']
 })
 export class MomentComponent {
+  imagePath: string = "http://localhost:3333/"; 
   moment?: Moment;
   
   constructor(private momentService: MomentService,
@@ -18,9 +19,10 @@ export class MomentComponent {
   ngOnInit(){
     //id que está na url
     const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
-
-    this.momentService.getMomentId(id)
-    .subscribe((item) => (this.moment = item.data));
+    this.momentService.getMomentId(id).subscribe((item) => {
+      (this.moment = item.data);
+      this.moment.created_at = new Date(this.moment.created_at!).toLocaleDateString('pt-BR') //linha convertendo a data para pt-br
+    })
   }
 
 }
